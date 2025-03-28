d981366a31f22f0d34c5abbc4c6a2e89ae60fb4a
Anderson <andavdtr@gmail.com>
2025-03-28 18:09:52 -0500

    validacion correo

--- src/singup.php
+++ src/singup.php
@@ -7,16 +7,39 @@
     $email  = $_POST['e_mail'];
     $passwd = $_POST['passw'];
 
+    //encriptado
     $enc_pass = md5($passwd);
+    //$enc_pass = sha1($passwd);
 
-    $sql = "INSERT INTO users (firstname, lastname, email, password)
-        VALUES('$fname','$lname','$email','$enc_pass')
-        ";
 
-        $res = pg_query($conn, $sql);
-        if($res){
-            echo "Users has been created succesfully";
+    //validar(antes de insert)
+    $sql_valid_mail = "
+        SELECT 
+            COUNT(email) as total
+        FROM
+            users
+        WHERE
+            email = '$email'
+            LIMIT 1
+    ";
+    $res = pg_query($conn, $sql_valid_mail);
+
+    if($res){
+        $row = pg_fetch_assoc($res);
+        if($row['total'] > 0){
+            echo "Email already exists";
         }else{
-            echo "Error";
+            $sql = "INSERT INTO users (firstname, lastname, email, password)
+            VALUES('$fname','$lname','$email','$enc_pass')
+            ";
+
+            $res = pg_query($conn, $sql);
+            if($res){
+                echo "Users has been created succesfully";
+            }else{
+                echo "Error";
+            }
         }
+    }
+
 ?>
