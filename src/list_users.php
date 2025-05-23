3c286cf56063f60ccfee01f39713fce933aa4dd4
Anderson <andavdtr@gmail.com>
2025-05-23 18:01:46 -0500

    con imagen

--- src/list_users.php
+++ src/list_users.php
@@ -10,11 +10,20 @@
     <title>Document</title>
 </head>
 <body>
-    <table border="1" align="Center">    
+    <table border="1" align="Center">
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>E-mail</th>
+            <th>Status</th>
+            <th>Photo</th>
+            <th>...</th>
+        </tr>    
         <?php
             //code
             $sql = "
-                select 
+                select
+                    id, 
                     firstname,
                     lastname,
                     email,
@@ -34,10 +43,11 @@
                 echo "<td>". $row['lastname'] ."</td>";
                 echo "<td>". $row['email'] ."</td>";
                 echo "<td>". $row['status'] ."</td>";
+                echo "<td alling='center'><img src='photo_users/photo_default.png' width='40'></td>";
                 echo "<td>";
                 echo "<a href=''><img src = 'icons/editar.png' width='20'></a>";
                 echo "<a href=''><img src = 'icons/simbolo-de-bote-de-basura-negro.png' width='20'></a>";
-                echo "<a href=''><img src = 'icons/simbolo-de-interfaz-de-lupa-de-busqueda.png' width='20'></a>";
+                echo "<a href='http://localhost/schoolar/src/delete.php'><img src = 'icons/simbolo-de-interfaz-de-lupa-de-busqueda.png' width='20'></a>";
                 echo "</td>";
                 echo "</tr>";
             }
